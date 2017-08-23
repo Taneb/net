36efb813870938512bcaec6acb28217a393d2a4a
= <nvd1234@gmail.com>
2017-08-23 18:35:12 +0100

    Add some IndexedTardis functions

--- IndexedTardis.hs
+++ IndexedTardis.hs
@@ -28,3 +28,29 @@ instance IxMonadFix (ITardis b f) where
     let (x, (bi, fj)) = runITardis (xtx x) (bj, fi)
     in (x, (bi, fj))
 
+getPast :: ITardis b f i i (f i)
+getPast = ITardis $ \(b, f) -> (f, (b, f))
+
+getFuture :: ITardis b f i i (b i)
+getFuture = ITardis $ \(b, f) -> (b, (b, f))
+
+sendPast :: b i -> ITardis b f i i ()
+sendPast b = ITardis $ \(_, f) -> ((), (b, f))
+
+sendFuture :: f i -> ITardis b f i i ()
+sendFuture f = ITardis $ \(b, _) -> ((), (b, f))
+
+getBoth :: ITardis b f i i (b i, f i)
+getBoth = ITardis $ \s -> (s, s)
+
+sendBoth :: (b i, f j) -> ITardis b f i j ()
+sendBoth s = ITardis $ \_ -> ((), s)
+
+modifyForwards :: (f i -> f i) -> ITardis b f i i ()
+modifyForwards k = ITardis $ \(b, f) -> ((), (b, k f))
+
+modifyBackwards :: (b i -> b i) -> ITardis b f i i ()
+modifyBackwards k = ITardis $ \(b, f) -> ((), (k b, f))
+
+modifyBoth :: (b j -> b i) -> (f i -> f j) -> ITardis b f i j ()
+modifyBoth kb kf = ITardis $ \(b, f) -> ((), (kb b, kf f))
